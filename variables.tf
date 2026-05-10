a539ec6124cc7a09b2d323b95f0b380882381365
lsooltan <liorsultan9@gmail.com>
2026-05-10 16:08:54 +0300

    fix: add default values for azure variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,24 @@
 variable "project_id" {
-  type = string
+  type        = string
+  description = "The name of the resource group"
+  default     = "production" # זה השם של ה-RG שיצרת באז'ור
 }
 
 variable "region" {
-  type    = string
-  default = "us-central1"
+  type        = string
+  default     = "East US" # אז'ור משתמש בשמות כמו East US ולא us-central1
 }
 
 variable "buckets" {
   type = map(object({
     labels = map(string)
   }))
+  default = {
+    "storage1" = {
+      labels = { "env" = "prod" }
+    },
+    "storage2" = {
+      labels = { "env" = "test" }
+    }
+  }
 }
