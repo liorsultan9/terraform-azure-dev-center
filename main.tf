57ac65f5a92bfab37d16bddea07cd4489895aefc
lsooltan <liorsultan9@gmail.com>
2026-05-10 12:52:35 +0300

    🚀 actually adding content to deployment file

--- main.tf
+++ main.tf
@@ -1,20 +1,26 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
+  }
+
+  # הוספת ה-Backend (הזיכרון) כדי שה-State יישמר באז'ור ולא אצלך במחשב
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-mgmt"   # שם ה-Resource Group שבו נמצא ה-Storage
+    storage_account_name = "tfstatelior2026"     # שם ה-Storage Account שיצרת עבור ה-State
+    container_name       = "tfstate"             # שם ה-Container
+    key                  = "terraform.tfstate"   # שם הקובץ שיווצר
   }
 }
 
-provider "google" {
-  project = var.project_id
-  region  = var.region
+provider "azurerm" {
+  features {} # חובה ב-Azure
 }
 
-resource "google_storage_bucket" "buckets" {
-  for_each = var.buckets
-  name     = each.key
-  location = var.region
-  labels   = each.value.labels
+# דוגמה ליצירת משאב באז'ור (במקום ה-Bucket של גוגל)
+resource "azurerm_resource_group" "example" {
+  name     = "rg-my-dev-center"
+  location = "West Europe"
 }
